3d2ea07cff805ceae48c44e72c6ace074cfa6d6e
Eric Zingeler <eezing@gmail.com>
2022-09-28 15:45:16 -0700

    save

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -41,35 +41,6 @@ program
   });
 
 program
-  .command('history')
-  .description('list of previous migrations')
-  .option('--host <PGHOST>', 'database server host')
-  .option('--port <PGPORT>', 'database server port')
-  .option('--user <PGUSER>', 'database user')
-  .option('--password <PGPASSWORD>', 'user password')
-  .option('--database <PGDATABASE>', 'database name')
-  .action(async (options) => {
-    await withSqergeErrorHandler(async () => {
-      const sql = postgres({
-        host: options.host,
-        port: parseInt(options.port),
-        user: options.user,
-        password: options.password,
-        database: options.database,
-        onnotice: () => {},
-      });
-
-      try {
-        await history(sql);
-      } catch (error) {
-        throw error;
-      } finally {
-        await sql.end();
-      }
-    });
-  });
-
-program
   .command('next')
   .description('list files in next migration')
   .argument('<dir>', 'path to directory of migration files')
@@ -90,7 +61,36 @@ program
       });
 
       try {
-        await next(sql, dir);
+        await next(sql, pathResolve(dir));
+      } catch (error) {
+        throw error;
+      } finally {
+        await sql.end();
+      }
+    });
+  });
+
+program
+  .command('history')
+  .description('list of previous migrations')
+  .option('--host <PGHOST>', 'database server host')
+  .option('--port <PGPORT>', 'database server port')
+  .option('--user <PGUSER>', 'database user')
+  .option('--password <PGPASSWORD>', 'user password')
+  .option('--database <PGDATABASE>', 'database name')
+  .action(async (options) => {
+    await withSqergeErrorHandler(async () => {
+      const sql = postgres({
+        host: options.host,
+        port: parseInt(options.port),
+        user: options.user,
+        password: options.password,
+        database: options.database,
+        onnotice: () => {},
+      });
+
+      try {
+        await history(sql);
       } catch (error) {
         throw error;
       } finally {
