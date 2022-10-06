a2b7dbd811b0ca0760a1a47462228a252dde18b9
Eric Zingeler <eezing@gmail.com>
2022-10-06 09:18:26 -0700

    added test

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -2,33 +2,29 @@ import { execSync } from 'child_process';
 import postgres, { Sql } from 'postgres';
 
 const PGHOST = 'localhost';
-const PGPORT = '5438';
+const PGPORT = 5438;
 const PGUSER = 'jonathan';
 const PGPASSWORD = 'iliketurtles';
 const PGDATABASE = 'dev';
 
-describe('migrate', () => {
+describe('execute migration', () => {
   let sql: Sql<{}>;
 
   beforeEach(() => {
     execSync('npm run dev:postgres');
 
     sql = postgres({
-      connection: {
-        host: PGHOST,
-        port: PGPORT,
-        user: PGUSER,
-        password: PGPASSWORD,
-        database: PGDATABASE,
-      },
+      host: PGHOST,
+      port: PGPORT,
+      user: PGUSER,
+      password: PGPASSWORD,
+      database: PGDATABASE,
     });
   });
 
-  afterEach(async () => {
-    await sql.end();
-  });
+  afterEach(() => sql.end());
 
-  test('examples/basic should execute successfully', async () => {
+  test('examples/basic should create person table with 1 row', async () => {
     // Arrange
     const command = `PGHOST=${PGHOST} PGPORT=${PGPORT} PGUSER=${PGUSER} PGPASSWORD=${PGPASSWORD} PGDATABASE=${PGDATABASE} sqerge ./examples/basic`;
 
@@ -37,5 +33,8 @@ describe('migrate', () => {
 
     // Assert
     expect(result.match(/executed/g)?.length).toBe(3);
+    expect(await sql`select * from person;`).toEqual([
+      { id: 1, name: 'Luke Skywalker', age: 21 },
+    ]);
   });
 });
